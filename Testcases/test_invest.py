6838222d6efbb9ccdeac64294f2d41cf9bd04222
ypsss <1719100539@qq.com>
2022-09-16 16:09:25 +0800

    优化  新增

---
+++ Testcases/test_invest.py
@@ -0,0 +1,51 @@
+import requests
+import unittest
+import os
+from unittestreport import ddt, list_data
+from common.handler_exce import HeadleExcel
+from common.heandler_path import DATA_DTR
+from common.headler_conf import conf
+from common.headler_log import my_log
+from common.heandler_tools import random_phone
+from common.heandler_tools import replace_data
+from common.heandler_fixture import BaseTest
+
+
+@ddt
+class TestInvest(unittest.TestCase, BaseTest):
+    exect = HeadleExcel(os.path.join(DATA_DTR, "testcase.xlsx"), "invest")
+    cases = exect.read_data()
+    test_url = conf.get('env', 'test_url')
+    header = eval(conf.get('env', "headers"))
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.admin_login()
+        cls.user_login()
+        cls.add_porject()
+
+    def setUp(self) -> None:
+        self.audit()
+
+    @list_data(cases)
+    def test_reqister(self, item):
+        url = self.test_url + item['url']
+        """数据替换"""
+        item["data"] = replace_data(item["data"], TestInvest)
+        params = eval(item['data'])
+        expected = eval(item["expected"])
+        method = item['method']
+        """请求接口"""
+        response = requests.request(method=method, url=url, json=params, headers=self.header)
+        res = response.json()
+        print("实际结果 ", res)
+        print("预期结果 ", expected)
+        try:
+            self.assertEqual(expected["code"], res["code"])
+            self.assertEqual(expected['msg'], res['msg'])
+        except AssertionError as e:
+            my_log.error("用例--【{}】---执行失败".format(item["title"]))
+            my_log.exception(e)
+            raise e
+        else:
+            my_log.info("用例--【{}】---执行通过".format(item["title"]))
